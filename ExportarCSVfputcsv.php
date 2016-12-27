4c97fc6331ae05fbc9888008806ef5528de8d676
Kike <kike@areavisual.net>
2016-12-27 11:25:13 +0100

    Repaso código
    Refresco código para ver si hago algún cambio.

--- ExportarCSVfputcsv.php
+++ ExportarCSVfputcsv.php
@@ -1,16 +1,19 @@
 <?php
     // Detalles de conexión a la base de datos
+
     $host = "localhost";
     $username = "root";
     $password = "root";
     $dbname = "BDRRHH";
 
     // Conectamos a la base de datos
-    $connection = mysqli_connect($host, $username, $password, $dbname) or die("Connection Error " . mysqli_error($connection));
 
-    $tildes = $connection->query("SET NAMES 'utf8'");
+    $connection = mysqli_connect($host, $username, $password, $dbname) or die("Error en la conexión: " . mysqli_error($connection));
+
+    //$tildes = $connection->query("SET NAMES 'utf8'");
 
     // extraemos los datos
+
     $sql = "select * from BDAltasCandi";
     $result = mysqli_query($connection, $sql) or die("Selection Error " . mysqli_error($connection));
 
@@ -24,5 +27,6 @@
     fclose($fp);
 
     //cerramos conexión
+
     mysqli_close($connection);
 ?>
